b344480f6a4698e67b933046de115b1a25136053
Victor Villacorta Plasencia <v.villacorta.unmsm@gmail.com>
2022-05-02 17:35:49 -0500

    ADD up days 12 and 13

---
+++ dia_12.py
@@ -0,0 +1,6 @@
+var_1 = 0
+var_2 = '-'
+
+print(id(var_1), id(var_2))
+var_1, var_2 = var_2, var_1
+print(id(var_1), id(var_2))
---
+++ dia_13.py
@@ -0,0 +1,4 @@
+var_1 = True
+var_2 = False
+
+print(not(var_2) or var_1)
